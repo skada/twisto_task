fe5fd97ba880261103503bda26124195fc79ece3
Jakub Skaryd <skaryd@gmail.com>
2017-10-23 12:30:34 +0200

    Add models to store tasks

--- ks/admin.py
+++ ks/admin.py
@@ -2,5 +2,39 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from ks.models import KSTask, InputItem, ResultItem
+
+
+class BaseItemInline(admin.TabularInline):
+    fields = ('index', 'value', 'weight')
+    can_delete = False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    readonly_fields = ('index', 'value', 'weight')
+
+
+class InputItemInline(BaseItemInline):
+    model = InputItem
+
+
+class ResultItemInline(BaseItemInline):
+    model = ResultItem
+
+
+@admin.register(KSTask)
+class KSTaskAdmin(admin.ModelAdmin):
+    list_display = ('task_id', 'start_time', 'end_time', )
+    readonly_fields = ('task_id', 'start_time', 'end_time', 'capacity', 'value', )
+    inlines = (ResultItemInline, InputItemInline, )
+    fieldsets = (
+        (_('Task base info'), {
+            'fields': ('task_id', ('capacity', 'value'), ('start_time', 'end_time'), )
+        }),
+    )
+
--- ks/models.py
+++ ks/models.py
@@ -2,5 +2,47 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class SackItem(models.Model):
+    index = models.PositiveIntegerField(_('Index'))
+    value = models.PositiveIntegerField(_('Value'))
+    weight = models.PositiveIntegerField(_('Weight'))
+
+    class Meta:
+        abstract = True
+
+
+class KSTask(models.Model):
+    task_id = models.UUIDField(_('Task ID'), blank=True, null=True)
+    start_time = models.DateTimeField(_('Start time'), auto_now_add=True)
+    end_time = models.DateTimeField(_('End time'), blank=True, null=True)
+    capacity = models.PositiveIntegerField(_('Capacity'))
+    value = models.PositiveIntegerField(_('Value'), blank=True, null=True)
+
+    @property
+    def num_items(self):
+        return len(self.items.all())
+
+    class Meta:
+        verbose_name = _('KnapSack task')
+        verbose_name_plural = _('KnapSack tasks')
+
+
+class InputItem(SackItem):
+    task = models.ForeignKey(KSTask, related_name='items')
+
+    class Meta:
+        ordering = ('task', 'index')
+        verbose_name = _('Input item')
+        verbose_name_plural = _('Input items')
+
+
+class ResultItem(SackItem):
+    task = models.ForeignKey(KSTask, related_name='results')
+
+    class Meta:
+        ordering = ('task', 'index')
+        verbose_name = _('Result item')
+        verbose_name_plural = _('Result items')
